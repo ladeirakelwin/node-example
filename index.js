91aa4f2b531c12f2b2a284eb5fda6b647c356e32
ladeirakelwin <ladeirakelwin@gmail.com>
2020-09-23 14:17:46 -0300

    Retirando node_modules

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World<h1>")
 })
 
-app.listen(process.env.PORT || 5000, () => console.log("Funfo!"))
+app.listen(port, () => console.log("Funfo!"))
